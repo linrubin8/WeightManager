458a3751d62e7581f70fe87a4654a30083333c24
LINRUBIN-PC\LINRUBIN <linrubin8@163.com>
2017-11-10 20:43:58 +0800

    删除fastreport

--- QuarryClient/FastReport/Data/WebServiceMethod.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Reflection;
-using FastReport.Utils;
-
-namespace FastReport.Data
-{
-}
---
+++ QuarryClient/LB.Controls/Controls/Report/ReportTemplateProcess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LB.Controls.Report
+{
+    public class ReportTemplateProcess
+    {
+        public static void CustomReportUpload(object sender,FastReport.Design.ReportUpdateFieldsEventArgs e)
+        {
+
+        }
+    }
+}
